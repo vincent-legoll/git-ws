334f0f3cee4d51475c316ee72232557d604a74b2
Vincent Legoll <vincent.legoll@iphc.cnrs.fr>
2024-12-10 18:45:40 +0100

    Whitespace fixes

--- exo_indent.py
+++ exo_indent.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-  	
-# on peut avoir        des commentaires moisis
-    	  
+
+# on peut avoir des commentaires moisis
+
 def affiche(message):
-  print('Vous avez reçu un message...')
-  print('  Voici le contenu:')
-  print(' ' + message)
-  
+    print('Vous avez reçu un message...')
+    print('  Voici le contenu:')
+    print('  ' + message)
+
 def main():
     affiche('Salut Jules!')
     affiche("Comment qu'c'est, gros ?")
-    
+
 if __name__ == '__main__':
-	main()
-  
+    main()
+
