b42fc74fc864eb2cca43fa017a7faea3640ca965
Vincent Legoll <vincent.legoll@iphc.cnrs.fr>
2024-12-10 18:32:26 +0100

    Init

---
+++ exo_indent.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+  	
+# on peut avoir        des commentaires moisis
+    	  
+def affiche(message):
+  print('Vous avez reçu un message...')
+  print('  Voici le contenu:')
+  print(' ' + message)
+  
+def main():
+    affiche('Salut Jules!')
+    affiche("Comment qu'c'est, gros ?")
+    
+if __name__ == '__main__':
+	main()
+  
